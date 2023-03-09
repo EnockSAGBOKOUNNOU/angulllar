361e4209ba3a2416e3195fc47df023d71bf47c80
Enock SAGBO KOUNNOU <enockkounou08@gmail.com>
2023-03-09 12:15:32 +0100

    mon_projjett

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ AppComponent,
+     BorderCardDirective, 
+     PokemonTypeColorPipe, 
+     ListPokemonComponent, 
+     DetailPokemonComponent],
+  imports: [ BrowserModule,AppRoutingModule
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  providers:[], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }
